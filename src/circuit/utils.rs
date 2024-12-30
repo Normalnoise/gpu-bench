a06705296596e67079c463998228a04e830eafdf
“Leo” <zhuiyi0403@163.com>
2024-12-30 16:50:59 +0800

    first commit

--- src/circuit/utils.rs
+++ src/circuit/utils.rs
@@ -1,5 +1,6 @@
 use blstrs::Scalar as Fr;
-use rand::{thread_rng, Rng};
+use ff::Field; // 引入 Field trait
+use rand::thread_rng;
 
 /// 生成指定大小的随机输入
 pub fn generate_random_inputs(size: usize) -> Vec<Option<Fr>> {
